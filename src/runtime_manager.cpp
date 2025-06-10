881bc07710d3629de71c1514c6ebf146b946375c
Procrastinaion <149471810+Procrastinaion@users.noreply.github.com>
2025-06-10 15:42:33 +0800

    Update runtime_manager.cpp

--- src/runtime_manager.cpp
+++ src/runtime_manager.cpp
@@ -4,7 +4,9 @@ RuntimeManager::RuntimeManager(MarkersManager &markers_manager, Camera *cam, Uti
     logger = std::make_shared<Logger>(setup->csv_logging_enabled, setup->parent_tf_name);
     markers = &markers_manager;
     markers->setLogger(logger);
+    detect_vis = std::make_unique<DetectionVisualizer>(cam->width, cam->height);
     detector = new DetectionAlgorithm(cam->width, cam->height, markers, logger);
+    detector->setDetectionVisualizer(detect_vis.get());
 }
 
 void RuntimeManager::start() {
